f1f3e439defb9c5b043ff2d91d626717b80708d0
abetoluwani <101352514+abetoluwani@users.noreply.github.com>
2024-05-10 05:27:27 -0400

    updating the read.me

---
+++ lib/app/routes/app_routes.dart
@@ -0,0 +1,14 @@
+part of 'app_pages.dart';
+// DO NOT EDIT. This is code generated via package:get_cli/get_cli.dart
+
+abstract class Routes {
+  Routes._();
+  static const HOMEPAGE = _Paths.HOMEPAGE;
+  static const SPLASHSCREEN = _Paths.SPLASHSCREEN;
+}
+
+abstract class _Paths {
+  _Paths._();
+  static const HOMEPAGE = '/homepage';
+  static const SPLASHSCREEN = '/splashscreen';
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'app/modules/splashscreen/bindings/splashscreen_binding.dart';
+import 'app/routes/app_pages.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
   runApp(const MyApp());
@@ -6,14 +10,19 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-    );
+    return ScreenUtilInit(
+        designSize: const Size(390, 844),
+        splitScreenMode: true,
+        builder: (context, _) {
+          return GetMaterialApp(
+            getPages: AppPages.routes,
+            debugShowCheckedModeBanner: false,
+            initialBinding: SplashscreenBinding(),
+            initialRoute: '/splashscreen',
+          );
+        });
   }
 }
